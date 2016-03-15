545b86962cb508cb1b99be0ad567d5d9fac0ce03
Samarth Gahire <samarthg@opera.com>
2016-03-15 13:57:03 +0000

    Setting start_date as "utcnow" is a wrong idea. One should always have a fixed start date not relative.

--- dags/airflow_unix_cron.py
+++ dags/airflow_unix_cron.py
@@ -10,17 +10,17 @@ default_args = {
     'owner': 'Samarth',
     'depends_on_past': False,
     'email': ['samarth.gahire@talentica.com'],
-    'start_date': datetime.utcnow() - timedelta(minutes=1),
+    'start_date': datetime(2016, 03, 15, 12),
     'email_on_failure': False,
 }
 
-dag = DAG('airflow_unix_cron', default_args=default_args, schedule_interval="20 * * * *")
+dag = DAG('airflow_unix_cron', default_args=default_args, schedule_interval="20,21,23 * * * *")
 
 t1 = BashOperator(
     task_id='print_date',
     bash_command='date',
     dag=dag)
 
-t1.doc_md = """\
+t1.doc_md = """
 #### Task Documentation
 """
